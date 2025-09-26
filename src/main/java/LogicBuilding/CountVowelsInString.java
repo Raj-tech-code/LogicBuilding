0635c89df0d0031df89c152bf84cf13f13a18f11
Rajasekar K <rajasekar.k0703@gmail.com>
2025-09-26 06:30:39 +0530

    Implemented logic for CountVowelsInString and CountVowelsInStringUsingSet

---
+++ src/main/java/LogicBuilding/CountVowelsInString.java
@@ -0,0 +1,24 @@
+package main.java.LogicBuilding;
+
+public class CountVowelsInString {
+
+    public static void main(String[] args){
+
+        String input = "Hello Word";
+        String vowels = "aeiouAEIOU";
+        int count = 0;
+
+        for(int index = 0; index < input.length(); index++){
+
+            if(vowels.indexOf(input.charAt(index))!=-1){
+
+                count++;
+            }
+
+        }
+
+        System.out.println("Vowels count in the String "+input+" is "+count);
+
+    }
+
+}
