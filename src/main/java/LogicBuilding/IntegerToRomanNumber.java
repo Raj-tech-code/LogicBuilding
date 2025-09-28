4fc09ac83c1b3c19441087f881be874b2b884fff
Rajasekar K <rajasekar.k0703@gmail.com>
2025-09-28 10:59:46 +0530

    Implemented logic for IntegerToRomanNumber

---
+++ src/main/java/LogicBuilding/IntegerToRomanNumber.java
@@ -0,0 +1,27 @@
+package main.java.LogicBuilding;
+
+public class IntegerToRomanNumber {
+
+    public static void main(String[] args){
+
+        int num = 22;
+        String[] romanArray = {"L","X","V","I"};
+        int[] romanCorrespondingNum = {50,10,5,1};
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < romanCorrespondingNum.length; i++){
+
+            while(num >= romanCorrespondingNum[i]){
+
+                num = num - romanCorrespondingNum[i];
+                sb.append(romanArray[i]);
+
+            }
+
+        }
+
+        System.out.println("Roman Number of Integer "+num+" is "+sb.toString());
+
+    }
+}
